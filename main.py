7407e53aea689fbce5b69cc3be4ef5851333a849
prathoseraaj <prathoseraaj0312@gmail.com>
2025-12-21 23:32:53 +0530

    feat: initialize the process

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import google.generativeai as genai
 import os 
 from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("gemini_api_key")
+genai.configure(api_key="gemini_api_key")
+
+
+    
