a383caaad69c1d4fd6faa1277900bbea6eaea546
prathoseraaj <prathoseraaj0312@gmail.com>
2025-12-23 23:30:05 +0530

    fix: instruction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 import os 
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 
@@ -8,16 +9,21 @@ gemini_api_key = os.getenv("gemini_api_key")
 genai.configure(api_key=gemini_api_key)
 
 instructions = (
-    "You are a mysterious Dungeon Master in a dark fantasy world. "
-    "Rules: 1. Keep descriptions under 3 sentences. 2. End every turn with 'What do you do?'. "
-    "3. Use the dice roll provided in the prompt to narrate success (15-20) or failure (1-10)."
+    "You are a Dungeon Master. Use only simple, basic English words. "
+    "Do not use difficult or poetic words. "
+    "Rules: 1. Use short sentences (max 10 words). "
+    "2. Use common words that a child or a beginner can understand. "
+    "3. End every turn with a simple question like 'What do you do?'. "
+    "4. Narrate success if the dice roll is 15-20 and failure if it is 1-10."
 )
 
 model = genai.GenerativeModel(model_name = 'gemini-2.5-flash', 
-                              instructions = instructions)
+                              system_instruction = instructions)
 
 chat = model.start_chat(history=[])
 initial_res = chat.send_message("Start the story in a rainy castle courtyard.")
+
+print(f"DM: {initial_res.text}\n")
 
 while True:
     
@@ -26,6 +32,11 @@ while True:
     if user_input.lower() in ["quit","exit","bye"]:
         break
     
-    response = chat.send_message(user_input)
+    roll = random.randint(1,20)
+    print(f"System: You Rolled {roll}")
+    
+    game_action = f"Player Action: {user_input}. Dice Roll result: {roll}"
+    
+    response = chat.send_message(game_action)
     
     print(response.text)
