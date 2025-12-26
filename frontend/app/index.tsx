363d4d0b5fc156e1260d82f586ab379764b2dcce
prathoseraaj <prathoseraaj0312@gmail.com>
2025-12-26 23:32:39 +0530

    feat: castle icon

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,14 +1,15 @@
 import { Text, View } from "react-native";
 import Navbar from "./components/Navbar";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 export default function App() {
   return (
     <View className="flex-1 bg-black">
       <Navbar />
 
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-xl font-bold text-blue-500">
-          Welcome to NativeWind!
+      <View className="flex-1 items-center mt-10 ">
+        <Text className=" text-[#d4af35]/30">
+          <MaterialIcons name="castle" size={60} />
         </Text>
       </View>
     </View>
