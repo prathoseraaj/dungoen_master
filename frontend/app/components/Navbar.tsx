c9226bd40f8f4facce62e86a6d9d72b5453ac090
prathoseraaj <prathoseraaj0312@gmail.com>
2025-12-26 23:19:32 +0530

    feat: navbar title

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -5,8 +5,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const Navbar = () => {
   return (
     <SafeAreaView>
-      <View className=" items-center">
-        <Text className="text-white">Navbar</Text>
+            <View className="items-center py-2">
+        <Text className="text-[#b89525] text-lg font-extrabold tracking-widest">
+          DUNGEON
+        </Text>
+        <Text className="text-[#b89525] text-xs font-bold tracking-[4px]">
+          TERMINAL
+        </Text>
       </View>
       <View className="h-[1.5px] mt-5 bg-[#b89525]" />
     </SafeAreaView>
