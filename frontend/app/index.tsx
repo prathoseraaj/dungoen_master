975210d74e4b856ae59ec2e43bc456954af02162
prathoseraaj <prathoseraaj0312@gmail.com>
2025-12-26 23:02:16 +0530

    feat: index

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,16 @@
 import { Text, View } from "react-native";
- 
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 bg-black">
+      <Navbar />
+
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to NativeWind!
+        </Text>
+      </View>
     </View>
   );
 }
