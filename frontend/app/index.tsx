80b323b103e31c70fd1d7ea09a7e0f01ad205f03
prathoseraaj <prathoseraaj0312@gmail.com>
2025-12-26 22:50:29 +0530

    fix: nativewind

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,8 @@
-import "./global.css"
 import { Text, View } from "react-native";
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center bg-black">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
